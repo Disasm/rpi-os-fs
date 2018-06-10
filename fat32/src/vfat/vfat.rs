49ca9cdd4bc7f60ee17e094d11fc368cf8cdc387
Vadim Kaushan <admin@disasm.info>
2018-06-11 00:39:04 +0300

    Iterator for FAT32 cluster chain

--- fat32/src/vfat/vfat.rs
+++ fat32/src/vfat/vfat.rs
@@ -38,27 +38,53 @@ impl<T: BlockDevice> VFat<T> {
         Ok(Shared::new(vfat))
     }
 
-    // TODO: The following methods may be useful here:
     //
     //  * A method to read from an offset of a cluster into a buffer.
     //
     fn read_cluster(
         &mut self,
         cluster: u32,
-        offset: usize,
+        offset: u64,
         buf: &mut [u8]
-    ) -> io::Result<usize> {
-        unimplemented!()
+    ) -> io::Result<()> {
+        if cluster < 2 {
+            return Err(io::Error::from(io::ErrorKind::InvalidInput));
+        }
+        let cluster_size_bytes = self.sectors_per_cluster as u64 * self.bytes_per_sector as u64;
+        if (offset + buf.len() as u64) > cluster_size_bytes {
+            return Err(io::Error::from(io::ErrorKind::InvalidInput));
+        }
+
+        let cluster_sector = self.data_start_sector + (cluster as u64 - 2) * self.sectors_per_cluster as u64;
+        let full_offset = cluster_sector * self.bytes_per_sector as u64 + offset as u64;
+        self.device.read_by_offset(full_offset, buf)
+    }
+
+
+    fn chain_iterator(&mut self, start_cluster: u32) -> ClusterChainIterator<T> {
+        ClusterChainIterator {
+            cluster: FatEntry(start_cluster),
+            vfat: self
+        }
     }
     //
     //  * A method to read all of the clusters chained from a starting cluster
     //    into a vector.
     //
-    //    fn read_chain(
-    //        &mut self,
-    //        start: Cluster,
-    //        buf: &mut Vec<u8>
-    //    ) -> io::Result<usize>;
+//    fn read_chain(
+//        &mut self,
+//        start_cluster: u32,
+//        buf: &mut Vec<u8>
+//    ) -> io::Result<usize> {
+//        let mut cluster_buffer = Vec::new();
+//        cluster_buffer.resize(self.sectors_per_cluster as usize * self.bytes_per_sector as usize, 0);
+//
+//        let mut current_cluster = start_cluster;
+//        loop {
+//            self.read_cluster(current_cluster, 0, &mut cluster_buffer)?;
+//            buf.extend_from_slice(&cluster_buffer);
+//        }
+//    }
     //
     //  * A method to return a reference to a `FatEntry` for a cluster where the
     //    reference points directly into a cached sector.
@@ -100,5 +126,32 @@ impl<'a, T: BlockDevice> FileSystem for &'a Shared<VFat<T>> {
 
     fn remove<P: AsRef<Path>>(self, _path: P, _children: bool) -> io::Result<()> {
         unimplemented!("read only file system")
+    }
+}
+
+struct ClusterChainIterator<'a, T: 'a + BlockDevice> {
+    vfat: &'a mut VFat<T>,
+    cluster: FatEntry,
+}
+
+impl<'a, T: BlockDevice> Iterator for ClusterChainIterator<'a, T> {
+    type Item = io::Result<u32>;
+
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        match self.cluster.status() {
+            Status::Data(cluster) => {
+                self.cluster = match self.vfat.fat_entry(cluster) {
+                    Ok(entry) => entry,
+                    Err(e) => return Some(Err(e))
+                };
+                Some(Ok(cluster))
+            }
+            Status::Eoc(_) => {
+                None
+            }
+            _ => {
+                Some(Err(io::Error::from(io::ErrorKind::InvalidData)))
+            }
+        }
     }
 }
